6d31dcee2de27e376855f5bd06f6596b3867aff4
gantzoulis <gantzoulis@hotmail.com>
2017-10-28 08:21:07 +0300

    [GA] Init upd

--- Unitest/Assets/init.cs
+++ Unitest/Assets/init.cs
@@ -7,8 +7,9 @@ public class init : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        Debug.Log("PP Init");
         Debug.Log("GA Init");
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
